5089b969b658a686117cc75602059e2071b91b5f
pbca26 <pbca26@gmail.com>
2019-07-31 20:47:46 +0300

    change paths to electrum-chips

--- electrum/gui/qt/__init__.py
+++ electrum/gui/qt/__init__.py
@@ -154,7 +154,7 @@ class ElectrumGui(Logger):
             submenu.addAction(_("Close"), window.close)
         m.addAction(_("Dark/Light"), self.toggle_tray_icon)
         m.addSeparator()
-        m.addAction(_("Exit Electrum"), self.close)
+        m.addAction(_("Exit Electrum CHIPS"), self.close)
 
     def tray_icon(self):
         if self.dark_icon:
--- electrum/util.py
+++ electrum/util.py
@@ -472,11 +472,11 @@ def user_dir():
     if 'ANDROID_DATA' in os.environ:
         return android_data_dir()
     elif os.name == 'posix':
-        return os.path.join(os.environ["HOME"], ".electrum")
+        return os.path.join(os.environ["HOME"], ".electrum-chips")
     elif "APPDATA" in os.environ:
-        return os.path.join(os.environ["APPDATA"], "Electrum")
+        return os.path.join(os.environ["APPDATA"], "Electrum-CHIPS")
     elif "LOCALAPPDATA" in os.environ:
-        return os.path.join(os.environ["LOCALAPPDATA"], "Electrum")
+        return os.path.join(os.environ["LOCALAPPDATA"], "Electrum-CHIPS")
     else:
         #raise Exception("No home directory found in environment variables.")
         return
