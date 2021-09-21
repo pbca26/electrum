f4af87573a7ac02c6aa12b1509f6e41be135989d
pbca26 <pbca26@gmail.com>
2021-09-21 19:06:39 +0300

    change app data path

--- electrum/util.py
+++ electrum/util.py
@@ -558,11 +558,11 @@ def user_dir():
     elif 'ANDROID_DATA' in os.environ:
         return android_data_dir()
     elif os.name == 'posix':
-        return os.path.join(os.environ["HOME"], ".electrum")
+        return os.path.join(os.environ["HOME"], ".electrum-sfusd")
     elif "APPDATA" in os.environ:
-        return os.path.join(os.environ["APPDATA"], "Electrum")
+        return os.path.join(os.environ["APPDATA"], "Electrum-SFUSD")
     elif "LOCALAPPDATA" in os.environ:
-        return os.path.join(os.environ["LOCALAPPDATA"], "Electrum")
+        return os.path.join(os.environ["LOCALAPPDATA"], "Electrum-SFUSD")
     else:
         #raise Exception("No home directory found in environment variables.")
         return
